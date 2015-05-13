a4e88f88576c4e19928d15d32fba160c072b5486
NassimRahali <nassim.rahali@gmail.com>
2015-05-13 14:35:28 +0200

    SQL
    insert_produit.sql - Dans la table DB_MARCHAND

---
+++ Script SQL/insert_produit.sql
@@ -0,0 +1,4 @@
+insert into PRODUITS values (1, 'Rossignol Experience 84 CA OPEN', 399);
+insert into PRODUITS values (2, 'SALOMON X PRO 120 2015', 459);
+insert into PRODUITS values (3, 'SCOTT COSMOS II 2015', 549);
+insert into PRODUITS values (4, 'DUPRAZ N SERIES', 599);
